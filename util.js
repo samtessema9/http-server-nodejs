69f8c85b84a14c678b1d45a8e6fe316a2f4a3edf
Sam Tessema <samtessema@Sams-MacBook-Air.local>
2025-04-12 12:17:10 -0700

    all challenges completed

--- util.js
+++ util.js
@@ -1,8 +1,18 @@
 function blockFor(ms) {
-    const start = Date.now();
-    while (Date.now() - start < ms) {
-      // Busy-wait loop (this blocks the event loop)
-    }
+  const start = Date.now();
+  while (Date.now() - start < ms) {
+    // Busy-wait loop (this blocks the event loop)
+  }
 }
 
-module.exports = blockFor;
+function parseAcceptEncodingHeader(encodings) {
+let encodingList = encodings.split(",")
+let trimmedList = encodingList.map(item => item.trim());
+
+return trimmedList
+}
+
+module.exports = {
+blockFor,
+parseAcceptEncodingHeader
+};
